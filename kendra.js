d59665b9d0addf5c9a517a683855b7bdf374b9eb
Blake Messer <blake@cratejoy.com>
2015-02-01 23:17:18 -0600

    js tab removal

--- kendra.js
+++ kendra.js
@@ -24,29 +24,31 @@ $(document).ready(function() {
     // Initiate the router
     var app_router = new AppRouter;
 	
-	// Map routes to page elements
-	var routeMap = {
-		"about": $('#about-content'),
-		"stories": $('#stories-content'),
-		"home": $('#home-content'),
-		"butts": $('#butts-content')
-	}
-	
-	// Hides all elements, then shows the new page
-	var $coverContainers = $('.cover-container');
-	function swapContent($incomingPage) {
-		$coverContainers.hide();
-		$incomingPage.show();
-	};
+    // Map routes to page elements
+    var routeMap = {
+        "about": $('#about-content'),
+        "stories": $('#stories-content'),
+        "home": $('#home-content'),
+        "butts": $('#butts-content')
+    }
+        	
+    // Hides all elements, then shows the new page
+    var $coverContainers = $('.cover-container');
+    function swapContent($incomingPage) {
+        $coverContainers.hide();
+        $incomingPage.show();
+    };
 
     app_router.on('route:defaultRoute', function(page) {
-		ga('send', 'pageview', page);
-		if (page in routeMap) {
-			swapContent(routeMap[page]);
-		} else {
-			swapContent(routeMap["home"]);
-		}
-    })
+        ga('send', 'pageview', page);  // tell ga about new page
+
+        if (page in routeMap) {
+            swapContent(routeMap[page]);
+        } else {
+            swapContent(routeMap["home"]);
+        }
+
+    });
 
     // History manages browser navigation (back button)
     Backbone.history.start();
